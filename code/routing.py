edb161184dcf4ba45acf4323d57f80cae407766b
Roope Savolainen <roope.savolainen@gmail.com>
2018-04-15 01:33:50 +0300

    Distances constructed correctly

--- code/routing.py
+++ code/routing.py
@@ -3,39 +3,57 @@ import numpy
 import scipy
 
 class Pathfinder:
-    weights = numpy.ndarray([0,0])
-    self.
+    dists = numpy.ndarray([0,0])
+    congestion = numpy.ndarray([0,0])
+    main_links = []
+    connectors = []
+    link_indices = {}
+    use_congestion = False
 
-    def __init__(self, vissim, congestion):
+    def __init__(self, vissim):
         self.vissim = vissim
-        self.update_weights()
-        self.congestion = congestion
 
         links = self.vissim.Net.Links.GetAll()
         self.main_links = filter(lambda c: int(c.AttValue('IsConn')) == 0, links)
         self.connectors = filter(lambda c: int(c.AttValue('IsConn')) == 1, links)
 
+        for i in range(len(self.main_links)):
+            no = unicode(self.main_links[i].AttValue('No'))
+            self.link_indices[no] = i
+
         size = len(self.main_links)
         self.weights = numpy.zeros([size, size])
+        self.congestion = numpy.zeros([size, size])
 
-        self.update_weights()
+        self.calculate_distances()
 
 
-    def update_weights(self):
-       
-       for conn in connectors:
-           src = int(conn.AttValue('FromLink')) - 1
-           dst = int(conn.AttValue('ToLink')) - 1
+    def calculate_distances(self):
+        for conn in self.connectors:
+            src = conn.AttValue('FromLink')
+            dst = conn.AttValue('ToLink')
 
-           length = float(main_links[src].AttValue('Length2D')) + float(conn.AttValue('Length2D'))
-           
-           if self.congestion:
-               coeff = _get_link_congestion(dst)
-               length *= coeff
+            length = float(self._get_link(src).AttValue('Length2D')) + float(conn.AttValue('Length2D'))
+            self.weights[self._get_link_index(dst), self._get_link_index(src)] = length
 
-            self.weights[src, dst] = length
+
+    def update_congestion(self):
+        for conn in self.connectors:
+            src = conn.AttValue('FromLink')
+            dst = conn.AttValue('ToLink')
+
+            coeff = _get_link_congestion(dst)
+            self.congestion[self._get_link_index(dst), self._get_link_index(src)] = coeff
 
 
     def _get_link_congestion(self, n):
-        link = self.main_links[n]
+        link = self._get_link(n)
         return 1.0 # TODO: implement congestion based coefficient
+
+
+    def _get_link(self, n):
+        index = self._get_link_index(n)
+        return self.main_links[index]
+
+    def _get_link_index(self, n):
+        return self.link_indices[n]
--- code/settings.py
+++ code/settings.py
@@ -1,2 +1,3 @@
 FILENAME = '\\data\\helsinki.inpx'
 COM_NAME = 'Vissim.Vissim.1000' # 1000 stands for Vissim v 10
+PATHFINDING_PERIOD = 20 # Update pathfinding once per 20 simulation secs
