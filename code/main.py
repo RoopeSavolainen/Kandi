cbefc5584f6235639684b52865ab80fea99ce6a8
Roope Savolainen <roope.savolainen@gmail.com>
2018-04-15 16:54:13 +0300

    Basic logging

--- code/main.py
+++ code/main.py
@@ -1,16 +1,22 @@
 import win32com.client as com
 import os
+from datetime import datetime
 
 from settings import *
 import routing
 
 def main():
     vissim = com.Dispatch(COM_NAME)
+    print_status('Vissim open')
+
     base_path = os.path.dirname(os.path.abspath(__file__))
     vissim.LoadNet(base_path + FILENAME)
+    print_status('Net loaded')
     
     use_congestion = False
     paths = routing.Pathfinder(vissim)
+
+    print_status('Pathfinding initialized')
     
     vissim.Simulation.RunSingleStep()
     paths.update_routes()
@@ -18,9 +24,17 @@ def main():
         if vissim.Simulation.AttValue('SimSec') % PATHFINDING_PERIOD == 0 and use_congestion:
             paths.update_congestion()
             paths.update_routes()
+            print_status('Congestion data and pathfinding updated')
 
         vissim.Simulation.RunSingleStep()
 
+    print_status('Simulation done')
+
+
+def print_status(msg):
+    t = datetime.now().strftime('%H:%M:%S')
+    print('[%s]: %s' % (t, msg))
 
 if __name__ == '__main__':
     main()
+
